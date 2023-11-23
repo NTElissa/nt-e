14d65045a51a0a097ab1149a1122216de120a4a0
NTElissa <122989293+NTElissa@users.noreply.github.com>
2023-11-23 02:23:23 +0200

    1 commite

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+
+mongoose.set('strictQuery', false);
+
+// configuring dotenv
+dotenv.config();
+
+// create server instance
+const app = express();
+
+// use of cors and body parse
+app.use(cors());
+app.use(bodyParser.json());
+//cookie for middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// route - home route
+app.get("/", (req, res) => {
+    res.status(200).send(`
+  <h1 style="text-align: center; margin-top: 20vh;"> Manager  event   APIS</h1>
+  `);
+});
+
+mongoose.set('strictQuery', false);
+let con = null;
+  con = mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+if(con){
+    console.log('Database has been connected')
+}
+
+
+export default app;
